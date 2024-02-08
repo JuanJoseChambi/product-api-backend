ed23e3cb65e78f0cd859a1632f3c0f21fe2164ad
Juan Jose <chambijuanjose05@gmail.com>
2024-02-08 16:50:56 -0300

    Bug Conect DB

--- Back/public/src/db.js
+++ Back/public/src/db.js
@@ -8,6 +8,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const { DB_USER, DB_HOST, DB_PORT, DB_NAME } = process.env;
 const db = mongoose_1.default.connect(`${DB_USER}://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+    // const db = mongoose.connect(`${DB_USER}://${DB_HOST}:${DB_PORT}/${DB_NAME}`)
     .then(() => console.log(`DB Conected to ${DB_NAME}`))
     .catch(err => console.log("Error al conctarse a MongoDB :", err));
 exports.default = db;
