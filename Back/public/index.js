340d5e603851a59e62c63ec35d01aa0a99e81d3b
Juan Jose <chambijuanjose05@gmail.com>
2024-02-08 15:22:23 -0300

    Bug DataProducts

--- Back/public/index.js
+++ Back/public/index.js
@@ -15,13 +15,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./src/app"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const db_1 = __importDefault(require("./src/db"));
-// import handlerUploadProductsData from "./src/utils/productsData";
+const productsData_1 = __importDefault(require("./src/utils/productsData"));
 dotenv_1.default.config();
 const { PORT } = process.env || 3001;
 try {
     app_1.default.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
         console.log(`Server listening on port ${PORT}`);
-        // handlerUploadProductsData()
+        (0, productsData_1.default)();
         yield db_1.default;
     }));
 }
